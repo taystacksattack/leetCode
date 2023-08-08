c6aad7ca983168ab1ef63f72871dbe7da8991d3a
Taylor McClerin <118297234+taystacksattack@users.noreply.github.com>
2023-08-08 17:43:28 -0400

    Time: 101 ms (96.53%) | Memory: 47.4 MB (54.37%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -3,17 +3,7 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    //create hash table for roman numeral values
-    const roman = {
-        "I" : 1,
-        "V" : 5,
-        "X" : 10,
-        "L" : 50,
-        "C" : 100,
-        "D" : 500,
-        "M" : 1000
-    }
-    
+ 
     //  declare result sum
     let result = 0
     //  loop over the string
