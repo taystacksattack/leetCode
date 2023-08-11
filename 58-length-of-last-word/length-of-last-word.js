7e52ffaf7c1e9b7c454f492edfdef66113effc67
Taylor McClerin <118297234+taystacksattack@users.noreply.github.com>
2023-08-11 16:45:52 -0400

    Time: 62 ms (28.28%) | Memory: 42.1 MB (31.53%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -2,19 +2,32 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    const words =  s.split(" ")
-    console.log(words)
-    for (let i = words.length - 1; i >= 0; i--){
-        const word = words[i]
-        console.log("word", word)
-        if (!word){
-            continue
-        } else{
-            console.log("here's the word returned", word )
-            return word.length
-        }
+// var lengthOfLastWord = function(s) {
+//     const words =  s.split(" ")
+//     for (let i = words.length - 1; i >= 0; i--){
+//         const word = words[i]
+//         console.log("word", word)
+//         if (!word){
+//             continue
+//         } else{
+//             return word.length
+//         }
 
+//     }
+//     // return (words[words.length-1]).length
+// };
+
+
+var lengthOfLastWord = function(s) {
+    let count = 0
+    // let words = s.split("")
+    for (let i = s.length - 1; i >= 0; i--){
+        let char = s[i]
+        let next = s[i-1]
+        // console.log(char)
+        if (char === " ") continue
+        count++
+        if (next === " ") break
     }
-    // return (words[words.length-1]).length
-};
+    return count
+}
