4b886046c2f12914e175bf785f5d08e2e634ff86
Taylor McClerin <118297234+taystacksattack@users.noreply.github.com>
2023-08-21 19:50:35 -0400

    Time: 46 ms (69.30%) | Memory: 16.5 MB (16.16%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,24 +1,16 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        p1 = m - 1  # Pointer for nums1
-        p2 = n - 1  # Pointer for nums2
-        p_merge = m + n - 1  # Pointer for merged array
-        
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] >= nums2[p2]:
-                nums1[p_merge] = nums1[p1]
-                p1 -= 1
-            else:
-                nums1[p_merge] = nums2[p2]
-                p2 -= 1
-            p_merge -= 1
-            
-        # If there are remaining elements in nums2, copy them to nums1
-        while p2 >= 0:
-            nums1[p_merge] = nums2[p2]
-            p2 -= 1
-            p_merge -= 1
+        nums2_length = 0
+        # while nums2_length <= len(nums2)-1:
+        for i in range (len(nums1)-1, -1, -1):
+            if nums2_length == len(nums2): break
+            print("nums1", nums1[i])
+            if nums1[i] == 0:
+                nums1[i] = nums2[nums2_length]
+                nums2_length += 1
+            # print("nums1 after removal",nums1)
 
+        nums1.sort()
 
         """
         Do not return anything, modify nums1 in-place instead.
