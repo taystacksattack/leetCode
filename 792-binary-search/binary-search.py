b534f7ebcadc51f19df406237ebe4d70409c9d25
Taylor McClerin <118297234+taystacksattack@users.noreply.github.com>
2023-08-29 13:58:03 -0400

    Time: 210 ms (88.23%) | Memory: 18 MB (28.93%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -3,7 +3,9 @@ class Solution:
         left = 0
         right = len(nums)-1
         while left <= right:
-            mid = (left + right) // 2 #using (right-left) means it uses integer division - avoids integer overflow rather than (right - left) //2
+            mid = (left + right) // 2 
+            #mid = left + (right-left) // 2
+            #using (right-left) means it uses integer division - avoids integer overflow rather than (right - left) //2
 
             if nums[mid] == target:
                 return mid
@@ -13,3 +15,23 @@ class Solution:
                 left = mid + 1
 
         return -1
+
+
+# here it is recursively:
+'''
+class Solution: 
+    def search(self, nums: List[int], target: int) -> int:
+        def recurse (left right):
+            if left >= right:
+                return right if unms[right] == target else -1
+            pivot = left + (right -left)//2
+
+            if nums[pivot] == target:
+                return pivot
+            elif target < nums[pivot]:
+                return recurse (left, pivot -1)
+            else:
+                return recurse(pivot +1, right)
+        
+        return recurse(0, len(nums)-1)
+'''
