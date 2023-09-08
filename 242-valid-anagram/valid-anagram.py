08b7de4b5eee73ddaccd35c1965911544564a5e6
Taylor McClerin <118297234+taystacksattack@users.noreply.github.com>
2023-09-08 14:17:55 -0400

    Time: 60 ms (43.90%) | Memory: 17.9 MB (5.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # split, then sort, then check if they're equal
-        sSorted = sorted(list(s))
-        tSorted = sorted(list(t))
-    
-        return sSorted == tSorted
+
+        return sorted(list(s)) == sorted(list(t))
