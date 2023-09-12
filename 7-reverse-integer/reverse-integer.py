f9aa182fc32a0c0fef3377c052372379a2ed3ec5
Taylor McClerin <118297234+taystacksattack@users.noreply.github.com>
2023-09-12 14:33:12 -0400

    Time: 38 ms (75.84%) | Memory: 16.3 MB (64.60%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,13 +1,9 @@
 class Solution:
     def reverse(self, x: int) -> int:
         # if x > 2**31 -1 or x <= -2**31: return 0
-        INT_MIN = -2**31
-        INT_MAX = 2**31 - 1
 
         # print(2**31)
 
-        if x < INT_MIN or x > INT_MAX:
-            return 0
         
         res = str(x)
         if res[0] == "-":
@@ -16,7 +12,7 @@ class Solution:
         else:
             reversed_x = int(res[::-1])
         
-        if reversed_x < INT_MIN or reversed_x > INT_MAX:
+        if reversed_x < -2**31 or reversed_x > 2**31 - 1:
             return 0
         else: return reversed_x
         
